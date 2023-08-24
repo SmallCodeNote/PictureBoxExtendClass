0b83a0c5d561fe8739f88036ed52a874122362a7
SmallCodeNote <132815163+SmallCodeNote@users.noreply.github.com>
2023-08-25 05:43:22 +0900

    Update PictureBoxEx.Designer.cs

--- PictureBoxEx.Designer.cs
+++ PictureBoxEx.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace PanelPictureBoxSet
+namespace PanelPictureBoxSet
 {
     partial class PictureBoxEx
     {
@@ -28,45 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.PictureBoxEx_Panel = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel_PictureBox = new System.Windows.Forms.Panel();
+            this.pictureBox_View = new System.Windows.Forms.PictureBox();
             this.button_LoadFile = new System.Windows.Forms.Button();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar_ExpaningFactor = new System.Windows.Forms.TrackBar();
             this.button_Reset = new System.Windows.Forms.Button();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown_ExpaningFactor = new System.Windows.Forms.NumericUpDown();
             this.label_MouseInfo = new System.Windows.Forms.Label();
-            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
-            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.vScrollBar_Panel = new System.Windows.Forms.VScrollBar();
+            this.hScrollBar_Panel = new System.Windows.Forms.HScrollBar();
             this.button_Center = new System.Windows.Forms.Button();
-            this.PictureBoxEx_Panel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel_PictureBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_View)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_ExpaningFactor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_ExpaningFactor)).BeginInit();
             this.SuspendLayout();
             // 
-            // PictureBoxEx_Panel
+            // panel_PictureBox
             // 
-            this.PictureBoxEx_Panel.Controls.Add(this.pictureBox1);
-            this.PictureBoxEx_Panel.Location = new System.Drawing.Point(1, 0);
-            this.PictureBoxEx_Panel.Name = "PictureBoxEx_Panel";
-            this.PictureBoxEx_Panel.Size = new System.Drawing.Size(481, 471);
-            this.PictureBoxEx_Panel.TabIndex = 0;
-            this.PictureBoxEx_Panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBoxEx_Panel_MouseDown);
-            this.PictureBoxEx_Panel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBoxEx_Panel_MouseUp);
+            this.panel_PictureBox.Controls.Add(this.pictureBox_View);
+            this.panel_PictureBox.Location = new System.Drawing.Point(1, 0);
+            this.panel_PictureBox.Name = "panel_PictureBox";
+            this.panel_PictureBox.Size = new System.Drawing.Size(481, 471);
+            this.panel_PictureBox.TabIndex = 0;
+            this.panel_PictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBoxEx_Panel_MouseDown);
+            this.panel_PictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBoxEx_Panel_MouseUp);
             // 
-            // pictureBox1
+            // pictureBox_View
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBoxEx_Panel_MouseDown);
-            this.pictureBox1.MouseEnter += new System.EventHandler(this.PictureBoxEx_Panel_MouseEnter);
-            this.pictureBox1.MouseLeave += new System.EventHandler(this.PictureBoxEx_Panel_MouseLeave);
-            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBoxEx_Panel_MouseMove);
-            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBoxEx_Panel_MouseUp);
+            this.pictureBox_View.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_View.Name = "pictureBox_View";
+            this.pictureBox_View.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox_View.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_View.TabIndex = 0;
+            this.pictureBox_View.TabStop = false;
+            this.pictureBox_View.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBoxEx_Panel_MouseDown);
+            this.pictureBox_View.MouseEnter += new System.EventHandler(this.PictureBoxEx_Panel_MouseEnter);
+            this.pictureBox_View.MouseLeave += new System.EventHandler(this.PictureBoxEx_Panel_MouseLeave);
+            this.pictureBox_View.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBoxEx_Panel_MouseMove);
+            this.pictureBox_View.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBoxEx_Panel_MouseUp);
             // 
             // button_LoadFile
             // 
@@ -79,18 +79,18 @@
             this.button_LoadFile.UseVisualStyleBackColor = true;
             this.button_LoadFile.Click += new System.EventHandler(this.button_LoadFile_Click);
             // 
-            // trackBar1
+            // trackBar_ExpaningFactor
             // 
-            this.trackBar1.Location = new System.Drawing.Point(2, 512);
-            this.trackBar1.Maximum = 1000;
-            this.trackBar1.Minimum = 10;
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(480, 69);
-            this.trackBar1.TabIndex = 2;
-            this.trackBar1.TickFrequency = 50;
-            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
-            this.trackBar1.Value = 100;
-            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            this.trackBar_ExpaningFactor.Location = new System.Drawing.Point(2, 512);
+            this.trackBar_ExpaningFactor.Maximum = 1000;
+            this.trackBar_ExpaningFactor.Minimum = 10;
+            this.trackBar_ExpaningFactor.Name = "trackBar_ExpaningFactor";
+            this.trackBar_ExpaningFactor.Size = new System.Drawing.Size(480, 69);
+            this.trackBar_ExpaningFactor.TabIndex = 2;
+            this.trackBar_ExpaningFactor.TickFrequency = 50;
+            this.trackBar_ExpaningFactor.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar_ExpaningFactor.Value = 100;
+            this.trackBar_ExpaningFactor.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
             // 
             // button_Reset
             // 
@@ -103,29 +103,29 @@
             this.button_Reset.UseVisualStyleBackColor = true;
             this.button_Reset.Click += new System.EventHandler(this.button_Reset_Click);
             // 
-            // numericUpDown1
+            // numericUpDown_ExpaningFactor
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(396, 550);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
+            this.numericUpDown_ExpaningFactor.Location = new System.Drawing.Point(396, 550);
+            this.numericUpDown_ExpaningFactor.Maximum = new decimal(new int[] {
             1000,
             0,
             0,
             0});
-            this.numericUpDown1.Minimum = new decimal(new int[] {
+            this.numericUpDown_ExpaningFactor.Minimum = new decimal(new int[] {
             10,
             0,
             0,
             0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(80, 25);
-            this.numericUpDown1.TabIndex = 4;
-            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.numericUpDown1.Value = new decimal(new int[] {
+            this.numericUpDown_ExpaningFactor.Name = "numericUpDown_ExpaningFactor";
+            this.numericUpDown_ExpaningFactor.Size = new System.Drawing.Size(80, 25);
+            this.numericUpDown_ExpaningFactor.TabIndex = 4;
+            this.numericUpDown_ExpaningFactor.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown_ExpaningFactor.Value = new decimal(new int[] {
             100,
             0,
             0,
             0});
-            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.numericUpDown_ExpaningFactor.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // label_MouseInfo
             // 
@@ -136,21 +136,21 @@
             this.label_MouseInfo.TabIndex = 5;
             this.label_MouseInfo.Text = "...";
             // 
-            // vScrollBar1
+            // vScrollBar_Panel
             // 
-            this.vScrollBar1.Location = new System.Drawing.Point(482, 0);
-            this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Size = new System.Drawing.Size(30, 471);
-            this.vScrollBar1.TabIndex = 6;
-            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            this.vScrollBar_Panel.Location = new System.Drawing.Point(482, 0);
+            this.vScrollBar_Panel.Name = "vScrollBar_Panel";
+            this.vScrollBar_Panel.Size = new System.Drawing.Size(30, 471);
+            this.vScrollBar_Panel.TabIndex = 6;
+            this.vScrollBar_Panel.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
             // 
-            // hScrollBar1
+            // hScrollBar_Panel
             // 
-            this.hScrollBar1.Location = new System.Drawing.Point(2, 474);
-            this.hScrollBar1.Name = "hScrollBar1";
-            this.hScrollBar1.Size = new System.Drawing.Size(466, 30);
-            this.hScrollBar1.TabIndex = 7;
-            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            this.hScrollBar_Panel.Location = new System.Drawing.Point(2, 474);
+            this.hScrollBar_Panel.Name = "hScrollBar_Panel";
+            this.hScrollBar_Panel.Size = new System.Drawing.Size(466, 30);
+            this.hScrollBar_Panel.TabIndex = 7;
+            this.hScrollBar_Panel.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
             // 
             // button_Center
             // 
@@ -167,21 +167,21 @@
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.Controls.Add(this.button_Center);
-            this.Controls.Add(this.hScrollBar1);
-            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.hScrollBar_Panel);
+            this.Controls.Add(this.vScrollBar_Panel);
             this.Controls.Add(this.label_MouseInfo);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.numericUpDown_ExpaningFactor);
             this.Controls.Add(this.button_Reset);
             this.Controls.Add(this.button_LoadFile);
-            this.Controls.Add(this.PictureBoxEx_Panel);
-            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.panel_PictureBox);
+            this.Controls.Add(this.trackBar_ExpaningFactor);
             this.Name = "PictureBoxEx";
             this.Size = new System.Drawing.Size(512, 582);
             this.SizeChanged += new System.EventHandler(this.PictureBoxEx_SizeChanged);
-            this.PictureBoxEx_Panel.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel_PictureBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_View)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_ExpaningFactor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_ExpaningFactor)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -189,15 +189,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel PictureBoxEx_Panel;
+        private System.Windows.Forms.Panel panel_PictureBox;
         private System.Windows.Forms.Button button_LoadFile;
-        private System.Windows.Forms.TrackBar trackBar1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar_ExpaningFactor;
+        private System.Windows.Forms.PictureBox pictureBox_View;
         private System.Windows.Forms.Button button_Reset;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown_ExpaningFactor;
         private System.Windows.Forms.Label label_MouseInfo;
-        private System.Windows.Forms.VScrollBar vScrollBar1;
-        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.VScrollBar vScrollBar_Panel;
+        private System.Windows.Forms.HScrollBar hScrollBar_Panel;
         private System.Windows.Forms.Button button_Center;
     }
 }
